6221afc560b1e66634970768d161f3ee4836df5e
Angelina <h1255513@gmail.com>
2018-11-15 22:53:29 -0500

    added test for validating time interval

--- validate_heart_rate.py
+++ validate_heart_rate.py
@@ -51,9 +51,3 @@ def validate_heart_rate(hr_data, time):
     except ValueError:
         logging.error("ValueError: Required fields format incorrect")
         return "ValueError"
-
-
-if __name__ == '__main__':
-    time=dt.datetime.now()
-    print([time])
-    print(str(time))
